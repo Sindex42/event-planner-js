3af742a7fc12675032884fa319b18ad5f6b855b7
Alina Goaga <goaga.alina@gmail.com>
2019-01-22 16:41:47 +0000

    Individual event plans can be added to the events class

--- spec/EventsSpec.js
+++ spec/EventsSpec.js
@@ -3,10 +3,18 @@
 
 describe('Events', function () {
   let events = new Events
+  let oneEvent = 'Event'
 
   describe('eventArray', function () {
     it('should have an empty array by default', function () {
       expect(events.eventArray.length).toEqual(0)
+    })
+  })
+
+  describe('add', function () {
+    it('should add an event to the array of events ', function () {
+      events.add(oneEvent);
+      expect(events.eventArray).toContain('Event')
     })
   })
 })
--- src/Events.js
+++ src/Events.js
@@ -2,4 +2,8 @@ class Events {
   constructor () {
     this.eventArray = []
   }
+
+  add (eventPlan) {
+    this.eventArray.push(eventPlan);
+  }
 }
