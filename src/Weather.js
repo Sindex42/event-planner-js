70e2d5068349d1a1bcc50687ca07f78bc30ef7bb
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-25 15:04:58 +0000

    Change get request to accept city name
    Co-Authored-By: Chris Hassan <CKKH@users.noreply.github.com>

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,8 @@
-var cityID = 2643743
+var cityID = 'London'
 var appID = config.APPID
 
 var request = new XMLHttpRequest()
-request.open('GET', `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&APPID=${appID}`, true)
+request.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${cityID}&APPID=${appID}`, true)
 
 request.onload = () => {
   console.log(request.response)
