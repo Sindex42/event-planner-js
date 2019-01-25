da05109cde7c8f31fbb82a14ef84114d08863432
CKKH <chriskkhassan@gmail.com>
2019-01-25 16:57:58 +0000

    For review

--- interface.js
+++ interface.js
@@ -1,13 +1,14 @@
-'use strict'
+"use strict"
 
 window.onload = () => {
-  let button = document.getElementById('button')
+  let button = document.getElementById("button")
+  let weatherButton = document.getElementById("weather-button")
   let eventList = new EventList()
-  let eventsDiv = document.getElementById('events')
+  let eventsDiv = document.getElementById("events")
   let deleteButton = document.getElementById("delete-button")
 
   const display = () => {
-    eventsDiv.innerHTML = ''
+    eventsDiv.innerHTML = ""
     let events = eventList.renderEventList()
     eventsDiv.appendChild(events)
   }
@@ -19,13 +20,13 @@ window.onload = () => {
   }
 
   button.onclick = () => {
-    let content = document.getElementById('textbox')
-    let date = document.getElementById('date')
-    let time = document.getElementById('time')
+    let content = document.getElementById("textbox")
+    let date = document.getElementById("date")
+    let time = document.getElementById("time")
 
     let eventPlan = new EventPlan(content.value, date.value, time.value)
     eventList.add(eventPlan)
-    content.value = date.value = time.value = ''
+    content.value = date.value = time.value = ""
     display()
   }
 
@@ -35,4 +36,14 @@ window.onload = () => {
     eventsDiv.innerHTML = "No events listed";
   }
 
+  // weatherButton.onclick = () => {
+  //   let city = document.getElementById("city")
+
+  //   let weatherDescription = weather.METHODNAME('city')
+
+  //   let weatherDiv = document.getElementById("weather")
+
+
+  //   weatherDiv.innerHTML = weatherDescription
+    
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,16 @@
-const getWeather = () => {
-  let cityID = 'London', appID = config.APPID
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityID}&APPID=${appID}`
-  let request = new XMLHttpRequest()
+// const getWeather = () => {
+//   let cityID = 'London', appID = config.APPID
+//   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityID}&APPID=${appID}`
+//   let request = new XMLHttpRequest()
 
-  request.open('GET', weatherUrl, true)
+//   request.open('GET', weatherUrl, true)
 
-  request.onload = () => {
-    let data = JSON.parse(request.response)
-    return data.weather[0].description
-  }
+//   request.onload = () => {
+//     let data = JSON.parse(request.response)
+//     return data.weather[0].description
+//   }
 
-  request.send()
-}
+//   request.send()
+// }
 
-getWeather()
+// getWeather()
